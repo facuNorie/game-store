02c73f8bbdb9fc825692e747a32fe7ba828393f5
Facu Noriega <noriegafacundo08@gmail.com>
2021-09-30 18:55:05 -0300

    Using nav

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useRoute, useLocation, Route } from "wouter";
+import Nav from "../components/Nav";
 import GameDetails from "../views/GameDetails";
 import Home from "../views/Home";
 
 export default function Routes() {
   return (
     <>
+      <Nav />
       <Route exact path="/">
         <Home />
       </Route>
