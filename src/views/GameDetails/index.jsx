b29d035e655de5fe8af8565fb5c5a89176d727d4
Facu Noriega <noriegafacundo08@gmail.com>
2021-09-30 18:56:17 -0300

    Add Game screenshots, requirements and aditional info and his style

--- src/views/GameDetails/index.jsx
+++ src/views/GameDetails/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useRoute, Link } from "wouter";
+import { useRoute } from "wouter";
 import { getGameById } from "../.././apis/getGameById";
 import {
   GameContainer,
@@ -10,19 +10,23 @@ import {
   ButtonSave,
   GameTitleContainer,
   GameDescriptionContainer,
+  GameScreenshotsContainer,
+  GameAdditionalInfoContainer,
+  AdditionalInfo,
+  GameRequirementsContainer,
+  Main,
 } from "./styles";
 export default function GameDetails() {
   const [gameDetails, setGameDetails] = useState([]);
+  const [readMore, setReadMore] = useState(true);
   const [, params] = useRoute("/:game");
   let idGame = params.game.split("-").pop();
 
   useEffect(() => {
     getGameById(idGame).then((data) => setGameDetails(data));
-    console.log(gameDetails);
   }, []);
   return (
-    <div>
-      <Link href="/">Home</Link>
+    <Main>
       <GameContainer className="row">
         <GameHeader className="col-sm-4">
           {gameDetails.length !== 0 ? (
@@ -48,18 +52,94 @@ export default function GameDetails() {
                 <h2>{gameDetails.title}</h2>
                 <p>{gameDetails.publisher}</p>
               </GameTitleContainer>
-
               <GameDescriptionContainer>
                 <h2>About {gameDetails.title}</h2> <hr />
-                <p>{gameDetails.short_description}</p>
-                <button>Read more +</button>
+                {readMore ? (
+                  <p>{gameDetails.description}</p>
+                ) : (
+                  <p>{gameDetails.short_description}</p>
+                )}
+                <button onClick={() => setReadMore(!readMore)}>
+                  {readMore ? " Read less -" : "Read more +"}
+                </button>
               </GameDescriptionContainer>
+              <GameScreenshotsContainer>
+                <h2>{gameDetails.title} Screenshots</h2> <hr />
+                <div>
+                  {gameDetails.screenshots.map((screenshot) => {
+                    return (
+                      <img
+                        src={screenshot.image}
+                        key={screenshot.id}
+                        alt="asd"
+                      />
+                    );
+                  })}
+                </div>
+              </GameScreenshotsContainer>
+              <GameAdditionalInfoContainer>
+                <h2>Additional Information</h2> <hr />
+                <AdditionalInfo>
+                  <div>
+                    <span>Developer</span>
+                    <p>{gameDetails.developer}</p>
+                  </div>
+                  <div>
+                    <span>Publisher</span>
+                    <p>{gameDetails.publisher}</p>
+                  </div>
+                  <div>
+                    <span>Genre</span>
+                    <p>{gameDetails.genre}</p>
+                  </div>
+                  <div>
+                    <span>Release Date</span>
+                    <p>{gameDetails.release_date}</p>
+                  </div>
+                  <div>
+                    <span>Platform</span>
+                    <p>{gameDetails.platform}</p>
+                  </div>
+                  <div>
+                    <span>Status</span>
+                    <p>{gameDetails.status}</p>
+                  </div>
+                </AdditionalInfo>
+              </GameAdditionalInfoContainer>
+              <GameRequirementsContainer>
+                <h2>
+                  Minimum System Requirements <span>(Windows)</span>
+                </h2>
+                <hr />
+                <AdditionalInfo Requirements>
+                  <div>
+                    <span>OS</span>
+                    <p>{gameDetails.minimum_system_requirements.os}</p>
+                  </div>
+                  <div>
+                    <span>Processor</span>
+                    <p>{gameDetails.minimum_system_requirements.processor}</p>
+                  </div>
+                  <div>
+                    <span>Memory</span>
+                    <p>{gameDetails.minimum_system_requirements.memory}</p>
+                  </div>
+                  <div>
+                    <span>Graphics</span>
+                    <p>{gameDetails.minimum_system_requirements.graphics}</p>
+                  </div>
+                  <div>
+                    <span>Storage</span>
+                    <p>{gameDetails.minimum_system_requirements.storage}</p>
+                  </div>
+                </AdditionalInfo>
+              </GameRequirementsContainer>
             </>
           ) : (
             <span></span>
           )}
         </GameSection>
       </GameContainer>
-    </div>
+    </Main>
   );
 }
--- src/views/GameDetails/styles.js
+++ src/views/GameDetails/styles.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
-export const Main = styled.div``;
+export const Main = styled.div`
+  margin-top: 50px;
+`;
 export const GameContainer = styled.div`
   width: 90%;
   height: 2000px;
   margin: 0 auto;
+  padding-top: 50px;
 `;
 export const GameHeader = styled.div`
   height: 100%;
@@ -16,7 +19,7 @@ export const GameHeader = styled.div`
       margin-bottom: 10px;
     }
     position: sticky;
-    top: 0px;
+    top: 75px;
   }
 `;
 
@@ -60,11 +63,11 @@ export const ButtonSave = styled.div`
 export const GameSection = styled.div`
   height: 100%;
   padding: 0 10px;
-  border: 1px solid #444;
   color: #eeeeee;
 `;
 
 export const GameTitleContainer = styled.div`
+  margin-bottom: 50px;
   h2 {
     font-size: 2.5rem;
   }
@@ -74,11 +77,91 @@ export const GameTitleContainer = styled.div`
   }
 `;
 export const GameDescriptionContainer = styled.div`
+  margin-bottom: 50px;
+  h2 {
+    margin-bottom: 10px;
+  }
   hr {
     border: 1px solid #444;
     border-top: none;
+    margin-bottom: 20px;
   }
   p {
     color: #bbb;
+  }
+  button {
+    color: #eeeeee;
+    background-color: transparent;
+    border-radius: 3px;
+    cursor: pointer;
+    border: none;
+    padding: 10px;
+    -webkit-box-shadow: 0px 3px 30px 0px rgba(0, 0, 0, 0.5);
+    -moz-box-shadow: 0px 3px 30px 0px rgba(0, 0, 0, 0.5);
+    box-shadow: 0px 3px 30px 0px rgba(0, 0, 0, 0.5);
+  }
+`;
+export const GameScreenshotsContainer = styled.div`
+  margin-bottom: 50px;
+  h2 {
+    margin-bottom: 10px;
+  }
+  hr {
+    border: 1px solid #444;
+    border-top: none;
+    margin-bottom: 30px;
+  }
+  div {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    img {
+      cursor: zoom-in;
+      width: 33%;
+      margin-bottom: 4px;
+      transition: transform 0.5s ease;
+      &:hover {
+        transform: scale(1.5);
+      }
+    }
+  }
+`;
+export const GameAdditionalInfoContainer = styled.div`
+  margin-bottom: 50px;
+  h2 {
+    margin-bottom: 10px;
+  }
+  hr {
+    border: 1px solid #444;
+    border-top: none;
+    margin-bottom: 20px;
+  }
+`;
+export const AdditionalInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: ${(props) =>
+    props.Requirements ? "inherit" : "space-between"};
+  div {
+    width: ${(props) => (props.Requirements ? "50%" : "33%")};
+    margin-bottom: 10px;
+    span {
+      color: #bbb;
+    }
+  }
+`;
+export const GameRequirementsContainer = styled.div`
+  h2 {
+    margin-bottom: 10px;
+    span {
+      color: #bbb;
+    }
+  }
+  hr {
+    border: 1px solid #444;
+    border-top: none;
+    margin-bottom: 20px;
   }
 `;
