fdb21492e97f457a194ec494ca0943211239f4c0
Facu Noriega <noriegafacundo08@gmail.com>
2021-11-06 00:50:45 -0300

    Added /saved on route

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { Link } from "wouter";
-import { Navbar } from "./style";
+import React from 'react';
+import { Link } from 'wouter';
+import { Navbar } from './style';
 
 export default function Nav() {
   return (
     <Navbar>
-      <Link href="/">Home</Link>
+      <Link href='/'>Home</Link>
+      <Link href='/saved'>Saved</Link>
     </Navbar>
   );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { useRoute, useLocation, Route } from "wouter";
-import Nav from "../components/Nav";
-import GameDetails from "../views/GameDetails";
-import Home from "../views/Home";
+import React from 'react';
+import { useRoute, useLocation, Route } from 'wouter';
+import Nav from '../components/Nav';
+import GameDetails from '../views/GameDetails';
+import Home from '../views/Home';
+import Saved from '../views/Saved';
 
 export default function Routes() {
   return (
     <>
       <Nav />
-      <Route exact path="/">
+      <Route exact path='/'>
         <Home />
       </Route>
-      <Route path="/:game">
+      <Route exact path='/saved'>
+        <Saved />
+      </Route>
+      <Route path='/game/:game'>
         <GameDetails />
       </Route>
     </>
